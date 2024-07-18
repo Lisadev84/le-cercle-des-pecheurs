fd15484fdfdc668f8e6a72d2c00ce3a3116c8145
Isabelle Moriamez <lisadev84@gmail.com>
2024-07-18 18:47:17 +0200

    fix errors path head  and header

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,28 +4,28 @@ $currentUser = $currentUser ?? false;
 
 
 <header>
-    <a href="/app/index.php" class="logo">Le cercle des pêcheurs</a>
+    <a href="/index.php" class="logo">Le cercle des pêcheurs</a>
     <div class="header-mobile">
         <div class="header-mobile-icon">
-            <img src="/app/public/images/menu-burger-horizontal-bold.svg" alt="menu-mobile">
+            <img src="/public/images/menu-burger-horizontal-bold.svg" alt="menu-mobile">
         </div>
         <ul class="header-mobile-list">
             <?php if ($currentUser) : ?>
                 <li class=<?= $_SERVER['REQUEST_URI'] === '/app/form-article.php' ? 'active' : '' ?>>
-                    <a href="/app/form-article.php">Ecrire un article</a>
+                    <a href="/form-article.php">Ecrire un article</a>
                 </li>
                 <li>
-                    <a href="/app/auth-logout.php">Déconnexion</a>
+                    <a href="/auth-logout.php">Déconnexion</a>
                 </li>
-                <li class="<?= $_SERVER['REQUEST_URI'] === '/app/profile.php' ? 'active' : '' ?>">
-                    <a href="/app/profile.php">Mon espace</a>
+                <li class="<?= $_SERVER['REQUEST_URI'] === '/profile.php' ? 'active' : '' ?>">
+                    <a href="/profile.php">Mon espace</a>
                 </li>
             <?php else : ?>
-                <li class=<?= $_SERVER['REQUEST_URI'] === '/app/auth-register.php' ? 'active' : '' ?>>
-                    <a href="/app/auth-register.php">Inscription</a>
+                <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? 'active' : '' ?>>
+                    <a href="/auth-register.php">Inscription</a>
                 </li>
-                <li class=<?= $_SERVER['REQUEST_URI'] === '/app/auth-login.php' ? 'active' : '' ?>>
-                    <a href="/app/auth-login.php">Connexion</a>
+                <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? 'active' : '' ?>>
+                    <a href="/auth-login.php">Connexion</a>
                 </li>
             <?php endif; ?>
         </ul>
@@ -33,21 +33,21 @@ $currentUser = $currentUser ?? false;
     </div>
     <ul class="header-menu">
         <?php if ($currentUser) : ?>
-            <li class=<?= $_SERVER['REQUEST_URI'] === '/app/form-article.php' ? 'active' : '' ?>>
-                <a href="/app/form-article.php">Ecrire un article</a>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
+                <a href="/form-article.php">Ecrire un article</a>
             </li>
             <li>
-                <a href="/app/auth-logout.php">Déconnexion</a>
+                <a href="/auth-logout.php">Déconnexion</a>
             </li>
-            <li class="<?= $_SERVER['REQUEST_URI'] === '/app/profile.php' ? 'active' : '' ?> header-profile">
-                <a href="/app/profile.php"><?= $currentUser['firstname'][0] . $currentUser['lastname'][0] ?></a>
+            <li class="<?= $_SERVER['REQUEST_URI'] === '/profile.php' ? 'active' : '' ?> header-profile">
+                <a href="/profile.php"><?= $currentUser['firstname'][0] . $currentUser['lastname'][0] ?></a>
             </li>
         <?php else : ?>
-            <li class=<?= $_SERVER['REQUEST_URI'] === '/app/auth-register.php' ? 'active' : '' ?>>
-                <a href="/app/auth-register.php">Inscription</a>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? 'active' : '' ?>>
+                <a href="/auth-register.php">Inscription</a>
             </li>
-            <li class=<?= $_SERVER['REQUEST_URI'] === '/app/auth-login.php' ? 'active' : '' ?>>
-                <a href="/app/auth-login.php">Connexion</a>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? 'active' : '' ?>>
+                <a href="/auth-login.php">Connexion</a>
             </li>
         <?php endif; ?>
     </ul>
