b17b532a9b7b2de205b0f6597fcb923681761fbe
Isabelle Moriamez <lisadev84@gmail.com>
2024-07-19 12:50:07 +0200

    ajout meta name header

--- app/includes/head.php
+++ app/includes/head.php
@@ -1,5 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA Compatible" content="IE=edge">
+<meta name="description" content="Pêche, partage de pratique de pêche">
+<meta name="keyword" content="articles, matériel,respect de la pratique, astuce, histoire">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/public/css/style.css">
 <script defer src="/public/js/index.js"></script>
